d46b2ed68c1ec11046165158f9ac509d61a641be
Seth Berke <sethberke@gmail.com>
2024-03-07 13:52:20 -0500

    Delete Command Line Tools directory

--- Command Line Tools/Merge | R + DataTable/File Requirement | input.R
+++
@@ -1,4 +0,0 @@
-columns_path="$(inputs.input_columns.path)"
-results_path="$(inputs.input_results.path)"
-id_path ="$(inputs.input_id)"
-MAF_path = "$(inputs.input_MAF.path)"
--- Command Line Tools/gTDT | R + Bioconductors/File Requirement | input.R
+++
@@ -1,3 +0,0 @@
-vcf_path="$(inputs.input_vcf.path)"
-ped_path="$(inputs.input_ped.path)"
-sex_path="$(inputs.input_sex.path)"
